2c96dba344ca37653be2258e7910273430e30582
Rick Dias <rickelmegadias@gmail.com>
2022-02-11 23:50:15 -0300

    Solved exercise

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
   const {username} = request.headers;
   const user = users.find(user => user.username == username);
 
@@ -25,7 +24,6 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
   const { user } = request;
   if (user.pro == true ||
      (user.pro == false && user.todos.length < 10)) {
@@ -36,7 +34,6 @@ function checksCreateTodosUserAvailability(request, response, next) {
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
   const {username} = request.headers;
   const {id} = request.params;
 
@@ -63,7 +60,6 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
   const {id} = request.params;
   const user = users.find((user) => user.id === id);
 
